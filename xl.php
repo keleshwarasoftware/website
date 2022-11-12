9ecc7f0fd370394fc82e9730522fcb9d4168bbcc
Sreejith <sreejith.m.a@moonhive.in>
2022-11-12 16:29:52 +0530

    committed for the change

--- xl.php
+++ xl.php
@@ -1,4 +1,4 @@
 <?php
-$dree = file_get_contents("http://localhost/demo.wpuidevs.com/finaxio/team-details.html");
-print_r($dree);
+$sree = file_get_contents("http://localhost/demo.wpuidevs.com/finaxio/team-details.html");
+print_r($sree);
 ?>
